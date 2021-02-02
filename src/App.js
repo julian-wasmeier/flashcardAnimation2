200fa0a36de138fc52b1a7929fad0a2b6e93a598
julian-wasmeier <juli@wasmeier.com>
2021-02-02 16:17:24 +0100

    button extern

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import "./App.css";
+import styles from "./components/Card.module.css";
+import { useState } from "react";
 import Card from "./components/Card";
 function App() {
   const items = [
-    { title: "Jan" },
-    { title: "Peter" },
-    { title: "Jannis" },
-    { title: "Paul" },
+    { id: 1, title: "Jan" },
+    { id: 2, title: "Peter" },
+    { id: 3, title: "Jannis" },
+    { id: 4, title: "Paul" },
   ];
+  const [current, setCurrent] = useState(items.length + 1);
+
+  const nextHandler = () => {
+    setCurrent((prev) => prev - 1);
+  };
+
+  const prevHandler = () => {
+    if (current < items.length + 1) {
+      setCurrent((prev) => prev + 1);
+    }
+  };
+  console.log(current);
   return (
     <div className="App">
-      {items.map((item) => (
-        <Card title={item.title}></Card>
+      <button className="prevButton" onClick={prevHandler}>
+        PREV
+      </button>
+      {items.map((item, index) => (
+        <Card
+          current={current}
+          index={index}
+          id={item.id}
+          title={item.title}
+          nextHandler={nextHandler}
+          prevHandler={prevHandler}
+        ></Card>
       ))}
+      <button className="nextButton" onClick={nextHandler}>
+        NEXT
+      </button>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,36 +1,44 @@
 import React, { useState, useRef } from "react";
 import { useSpring, animated, config, useChain } from "react-spring";
+import styles from "./Card.module.css";
+
 function Card(props) {
   const [toggle, set] = useState();
   const leave = useSpring({
     // config: { friction: 200, tension: 200, mass: 4 },
-    config: { ...config.stiff, duration: 900 },
+    config: { duration: 600 },
 
-    transform: toggle
-      ? "translate(1500vw) scale(0.8)"
-      : "translate(0px) scale(1)",
+    transform:
+      props.current <= props.id || toggle
+        ? "translate(1500vw) scale(0.5) rotateY(100deg) "
+        : "translate(0px) scale(1) rotateY(0deg)",
   });
 
-  const springRef = useRef();
-
   const fade = useSpring({
-    config: { duration: 300 },
-    opacity: toggle ? "0" : "1",
+    config: { duration: 300, initial: null },
+
+    opacity: props.current <= props.id || toggle ? "0" : "1",
   });
 
   return (
-    <div>
-      <animated.div className="container" style={{ ...leave, ...fade }}>
-        <h1>{props.title}</h1>
-        <p>
-          Aute magna veniam dolor consequat elit quis Lorem culpa sint proident
-          consequat. Incididunt commodo do esse ex consequat deserunt eiusmod
-          sit eiusmod magna proident aliqua et laborum. Anim esse ullamco ipsum
-          in non nulla.
-        </p>
-        <button onClick={() => set(!toggle)}>Next</button>
-      </animated.div>
-    </div>
+    <animated.div
+      className={styles.cardContainer}
+      style={{ ...leave, ...fade }}
+    >
+      <h1>{props.title}</h1>
+      <p>
+        Aute magna veniam dolor consequat elit quis Lorem culpa sint proident
+        consequat. Incididunt commodo do esse ex consequat deserunt eiusmod sit
+        eiusmod magna proident aliqua et laborum. Anim esse ullamco ipsum in non
+        nulla.
+      </p>
+      <button className={styles.nextButton} onClick={props.nextHandler}>
+        NEXT
+      </button>
+      <button className={styles.prevButton} onClick={props.prevHandler}>
+        PREV
+      </button>
+    </animated.div>
   );
 }
 
