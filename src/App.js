4833f0109b03272c216765482f223a14a5d6ac24
julian-wasmeier <juli@wasmeier.com>
2021-02-13 00:53:47 +0100

    Upgrade

--- src/App.js
+++ src/App.js
@@ -1,59 +1,81 @@
 import "./App.css";
-import styles from "./components/Card.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Card from "./components/Card";
-import Test from "./components/Test";
 
 function App() {
   const items = [
     { id: 1, title: "Jan" },
     { id: 2, title: "Peter" },
     { id: 3, title: "Jannis" },
-    { id: 4, title: "Paul" },
+    { id: 4, title: "Jannis" },
+    { id: 5, title: "Jannis" },
+    { id: 6, title: "Jannis" },
   ];
-  const [current, setCurrent] = useState(items.length + 1);
+  const [current, setCurrent] = useState(items.length);
+  const [isForward, setForward] = useState(true);
 
   const nextHandler = () => {
-    if (current > 1) {
+    if (current > 0) {
       setCurrent((prev) => prev - 1);
     }
   };
 
   const prevHandler = () => {
-    if (current < items.length + 1) {
+    if (current < items.length && current != items.length) {
       setCurrent((prev) => prev + 1);
     }
   };
-  console.log(current);
+
+  const handler = isForward ? nextHandler : prevHandler;
+
+  // useEffect(() => {
+  //   const infinite1 = setInterval(() => {
+  //     handler();
+  //   }, 850);
+  //   return function cleanup() {
+  //     clearInterval(infinite1);
+  //   };
+  // }, [isForward]);
+
   return (
     <div className="App">
       <button
-        disabled={current == items.length + 1}
+        disabled={current === items.length}
         className="prevButton"
         onClick={prevHandler}
       >
         PREV
       </button>
-      {items.map((item, index) => (
-        <Card
-          itemsLength={items.length}
-          disabledPrev={current == items.length + 1}
-          disabledNext={current == 1}
-          curr={current}
-          index={index}
-          id={item.id}
-          title={item.title}
-          nextHandler={nextHandler}
-          prevHandler={prevHandler}
-        ></Card>
-      ))}
       <button
-        disabled={current == 1}
+        onClick={() => {
+          setForward(!isForward);
+        }}
+      >
+        {isForward ? "Backwards" : "Forwards"}
+      </button>
+      <button
+        disabled={current === 0}
         className="nextButton"
         onClick={nextHandler}
       >
         NEXT
       </button>
+      <div className="cardStack">
+        {items.map((item, index) => (
+          <Card
+            itemsLength={items.length}
+            disabledPrev={current === items.length}
+            disabledNext={current === 0}
+            curr={current}
+            index={index}
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            nextHandler={nextHandler}
+            prevHandler={prevHandler}
+          ></Card>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,20 @@
-import React, { useState, useRef } from "react";
+import React, { useRef, useState } from "react";
+import ReactCardFlip from "react-card-flip";
 import { useSpring, animated, config, useChain } from "react-spring";
 import styles from "./Card.module.css";
 
 function Card(props) {
-  const [disabled, setDisabled] = useState(false);
-
+  const [cardStyle, setCardStyle] = useState();
+  const [isFlipped, setIsFlipped] = useState();
   const upRef = useRef();
+  console.log(props.index);
+  console.log(props.curr);
   const up = useSpring({
     config: { ...config.stiff, friction: 40 },
+
     transform:
-      props.curr <= props.id
-        ? "translate(25px,-25px) "
+      props.curr <= props.index
+        ? "translate(150px,-10px)  "
         : "translate(0px, 0px) ",
     ref: upRef,
   });
@@ -20,64 +24,147 @@ function Card(props) {
     // config: { friction: 50, tension: 200, mass: 5 },
     config: { ...config.stiff, friction: 48, mass: 5, tension: 200 },
 
-    marginLeft: props.curr <= props.id ? 1000 : 0,
+    marginLeft: props.curr <= props.index ? 1000 : 0,
 
     ref: leaveRef,
   });
 
   const fadeRef = useRef();
   const fade = useSpring({
-    config: { duration: 400 },
+    config: { duration: 280 },
 
-    opacity: props.curr <= props.id ? "0" : "1",
+    opacity: props.curr <= props.index ? "0" : "1",
     ref: fadeRef,
   });
 
+  const CardStyle = () => {
+    if (props.index === props.curr - 1) {
+      return {
+        left: "0%",
+        transform: "scale(1) ",
+        filter: "contrast(100%) brightness(100%) saturate(100%)",
+      };
+    } else if (props.index === props.curr - 2) {
+      return {
+        left: "15%",
+        transform: "scale(0.914) ",
+        filter: "contrast(65%) brightness(112%) saturate(80%)",
+      };
+    } else if (props.index === props.curr - 3) {
+      return {
+        left: "25%",
+        transform: "scale(0.852)",
+        filter: "contrast(30%) brightness(120%) saturate(60%) ",
+      };
+    } else if (props.index >= props.curr) {
+      return {
+        left: "0%",
+        transform: "scale(1) ",
+        filter: "contrast(100%) brightness(100%) saturate(100%)",
+      };
+    } else {
+      return {
+        left: "20%",
+        transform: "scale(0.852)",
+        filter: "contrast(30%) brightness(155%) saturate(30%) ",
+      };
+    }
+  };
+
+  const card1 = useSpring({
+    ...CardStyle(),
+  });
+
+  // const card2Ref = useRef();
+  // const card2 = useSpring({
+  //   top: props.index === props.curr - 2 ? " 30% " : "20%",
+  //   left: props.index === props.curr - 2 ? " 34%" : "40%",
+  // });
+
   useChain(
-    props.curr <= props.id
-      ? [upRef, leaveRef, fadeRef]
-      : [leaveRef, fadeRef, upRef],
-    [0, 0.2, 0.2]
+    [upRef, leaveRef, fadeRef],
+
+    [0, 0.1, 0.2]
   );
 
-  const nextHandler2 = () => {
-    props.nextHandler();
-    // setDisabled(true); broken
-  };
-
-  const prevHandler2 = () => {
-    props.prevHandler();
-    // setDisabled(false); broken
-  };
-
   return (
-    <animated.div
-      id="cardCon"
-      className={styles.cardContainer}
-      style={{ ...leave, ...up, ...fade }}
-    >
-      <div className={styles.contentContainer}>
-        <h1>{props.title}</h1>
-        <p>
-          Aute magna veniam dolor consequat elit quis Lorem culpa sint proident
-          consequat. Incididunt commodo do esse ex consequat deserunt eiusmod
-          sit eiusmod magna proident aliqua et laborum. Anim esse ullamco ipsum
-          in non nulla.
-        </p>
-      </div>
-      <div className={styles.buttonContainer}>
-        <button className={styles.nextButton} onClick={nextHandler2}>
-          NEXT
-        </button>
-        <button
-          disabled={props.itemsLength == props.id}
-          className={styles.prevButton}
-          onClick={prevHandler2}
-        >
-          PREV
-        </button>
-      </div>
-    </animated.div>
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      <animated.div
+        className={styles.cardContainer}
+        style={{ ...fade, ...leave, ...up, ...card1 }}
+      >
+        <div className={styles.contentContainer}>
+          <h1>{props.title}</h1>
+          <p>
+            Aute magna veniam dolor consequat elit quis Lorem culpa sint
+            proident consequat. Incididunt commodo do esse ex consequat deserunt
+            eiusmod sit eiusmod magna proident aliqua et laborum. Anim esse
+            ullamco ipsum in non nulla.
+          </p>
+        </div>
+        <div className={styles.buttonContainer}>
+          <button
+            disabled={props.itemsLength === props.index + 1}
+            className={styles.prevButton}
+            onClick={props.prevHandler}
+          >
+            PREV
+          </button>
+
+          <button
+            className={styles.husoButton}
+            onClick={() => setIsFlipped(!isFlipped)}
+          >
+            HUSO
+          </button>
+          <button
+            disabled={props.curr === 0}
+            className={styles.nextButton}
+            onClick={props.nextHandler}
+          >
+            NEXT
+          </button>
+        </div>
+      </animated.div>
+
+      <animated.div
+        className={styles.cardContainer}
+        style={{ ...leave, ...up, ...fade, ...card1 }}
+      >
+        <div className={styles.contentContainer}>
+          <h1>{props.title}</h1>
+          <p>
+            Aute magna veniam dolor consequat elit quis Lorem culpa sint
+            proident consequat. Incididunt commodo do esse ex consequat deserunt
+            eiusmod sit eiusmod magna proident aliqua et laborum. Anim esse
+            ullamco ipsum in non nulla.
+          </p>
+        </div>
+        <div className={styles.buttonContainer}>
+          <button
+            disabled={props.itemsLength === props.index + 1}
+            className={styles.prevButton}
+            onClick={props.prevHandler}
+          >
+            PREV
+          </button>
+
+          <button
+            className={styles.husoButton}
+            onClick={() => setIsFlipped(!isFlipped)}
+          >
+            HUSO
+          </button>
+          <button
+            disabled={props.curr === 0}
+            className={styles.nextButton}
+            onClick={props.nextHandler}
+          >
+            NEXT
+          </button>
+        </div>
+      </animated.div>
+    </ReactCardFlip>
   );
 }
 
